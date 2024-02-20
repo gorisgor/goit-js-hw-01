8258df728a533a20b2b5848e11db4383eea336f3
gorisgor <igor.matvienko1989@gmail.com>
2024-02-20 20:58:01 +0200

    task1added

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function makeTransaction(quantity, pricePerDroid) {
+  console.log(
+    `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+  );
+}
+makeTransaction(5, 3000);
+makeTransaction(3, 1000);
+makeTransaction(10, 500);
+
+makeTransaction(8, 700);
