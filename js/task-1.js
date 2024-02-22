e5cef9ca62c0746cdf56f948bb262edf77858302
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-02-22 08:03:49 +0200

    Update

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,9 @@
 function makeTransaction(quantity, pricePerDroid) {
-  console.log(
-    `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
-  );
-}
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
+  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+};
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
 
-makeTransaction(8, 700);
+
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
 function getElementWidth (content, padding, border) {
-    console.log (parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border))
+   return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border)
 };
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
+
